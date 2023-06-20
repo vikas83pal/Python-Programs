588526eeb5e570f9cc528cb7ea0eb9e2739d4119
vikas pal <110290006+vikas83pal@users.noreply.github.com>
2023-06-20 19:08:07 +0530

    Add files via upload

---
+++ 1_variable.py
@@ -0,0 +1,14 @@
+# variable - Is A Named Memory Location In Which Values Can Be Stored.And This Values can Be Changed During Exce Of The Program.
+
+age = 20 # Here age is a integer memory location and it is stored with a value 20 and it cannot be changed
+print("Your age is:",age)
+# print("Your age is:"+age) --error because you're typing to concatened the strings which is possible with typing casting.. which we will learn in upcominig classes!!!
+
+Name = "CodeWithPro" # Here Name is a String memory location and it is stored with a value CodeWithPro and it cannot be changed
+print("Your Name is:",Name)
+
+height = 250.0 # Here height is a flot memory location and it is stored with a value 250.0 and it cannot be changed
+print("Your Height is:",height)
+
+
+
